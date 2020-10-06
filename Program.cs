4be80edb864d1208a3cf54a49137de69a5e6daab
chuck francis <chuck_francis10@yahoo.com>
2020-10-06 14:57:44 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Enum1
+{
+    public enum ProductCodes
+    { 
+    Milk = 0,
+    juice = 1,
+    Tea = 2,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductCodes test = ProductCodes.Milk;
+            Console.WriteLine((int)test);
+
+            int test2 = 1;
+            Console.WriteLine((ProductCodes)test2);
+
+
+            Console.WriteLine(test.ToString());
+
+            string test3 = "Tea";
+            ProductCodes getParse;
+            bool checkParse = Enum.TryParse(test3, out getParse);
+            Console.WriteLine(getParse);
+        }   
+    }
+}
